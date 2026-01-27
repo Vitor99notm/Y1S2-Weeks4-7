2e56a6267225a20f74378133ac7b8d232f86ce4d
dasivito <dasilva.vitor.355@gmail.com>
2026-01-27 10:53:09 -0500

    Made a UI button that changes color, size and deactivates an object

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    float speed = 0.01f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPos = transform.position;
+        newPos.x += speed;
+        transform.position = newPos;
+
+
+        Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        if (screenPosition.x < 0 || screenPosition.x > Screen.width)
+        {
+            speed = speed * -1;
+        }
+    }
+}
