7cc672c8026ea26681bb8e610943a4585230a682
dasivito <dasilva.vitor.355@gmail.com>
2026-02-03 11:25:43 -0500

    Added slider code into UIDemo Script which shows the value to the current slide through Text

--- Assets/Scripts/UIDemo.cs
+++ Assets/Scripts/UIDemo.cs
@@ -9,15 +9,22 @@ public class UIDemo : MonoBehaviour
     public Image imageUI;
     public int countClick = 0;
     public TextMeshProUGUI score;
+    public Slider slider;
+    public TextMeshProUGUI sliderDisplay;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        score.text = countClick.ToString();
+        slider.wholeNumbers = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        sliderDisplay.text = slider.value.ToString();
+
         if (Keyboard.current.anyKey.wasPressedThisFrame == true)
            
         {
