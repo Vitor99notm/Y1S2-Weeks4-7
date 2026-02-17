3c8e97d5984208475b1e88607ae8029a2b9ebad8
Vitor99notm <dasilva.vitor.355@gmail.com>
2026-02-16 20:52:23 -0500

    Commenting

---
+++ Assets/Scripts/DestroyScript.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DestroyScript : MonoBehaviour
+{
+    //public List<GameObject> coffee;
+    public GameObject coffePrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Mouse.current.leftButton.wasPressedThisFrame == true)
+        //{
+            //coffee.Remove(coffePrefab);
+            //Destroy(coffePrefab);
+        //}
+    }
+}
--- Assets/Scripts/SliderRotate.cs
+++ Assets/Scripts/SliderRotate.cs
@@ -2,17 +2,6 @@ using UnityEngine;
 
 public class SliderRotate : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     //Public void so function can be accessed through buttons and/or sliders
     public void sliderRotate(float rotate)
