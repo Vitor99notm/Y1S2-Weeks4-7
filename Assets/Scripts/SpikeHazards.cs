eddb76aeba28100d20cc829ac407a2efed49cedd
dasivito <dasilva.vitor.355@gmail.com>
2026-02-17 11:26:13 -0500

    Made a timer so that the spikes can dissapear

--- Assets/Scripts/SpikeHazards.cs
+++ Assets/Scripts/SpikeHazards.cs
@@ -3,6 +3,9 @@ using UnityEngine.Events;
 
 public class SpikeHazards : MonoBehaviour
 {
+    public float currentTime = 0;
+    public float maxTime = 3;
+
     public SpriteRenderer spikes;
     public bool isInSpikes = false;
     public UnityEvent OnEnterSpikes;
@@ -16,7 +19,15 @@ public class SpikeHazards : MonoBehaviour
 
     void Update()
     {
-        if (spikes.bounds.Contains(transform.position) == true)
+        currentTime += Time.deltaTime;
+
+        if(currentTime > maxTime)
+        {
+            currentTime = 0;
+            spikes.gameObject.SetActive(!gameObject.activeInHierarchy);
+        }
+
+        if (spikes.bounds.Contains(transform.position) == true && spikes.gameObject.activeInHierarchy)
         {
             if (isInSpikes == true)
             {
